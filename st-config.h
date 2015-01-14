f2543bcfc095c2b4d49a7863f8331083cd576bca
Sam Rogerson <rogerson.samuel@gmail.com>
2015-01-14 00:54:50 +0000

    use color configs

--- st-config.h
+++ st-config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "../colors/solarized-light.h"
+
 /*
  * appearance
  *
@@ -47,44 +49,6 @@ static int bellvolume = 0;
 static char termname[] = "st-256color";
 
 static unsigned int tabspaces = 8;
-
-
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-};
-
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-static unsigned int defaultfg = 7;
-static unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
