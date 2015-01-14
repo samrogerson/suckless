3a805bdae9de789795c56a9cf0633c1283091485
Sam Rogerson <rogerson.samuel@gmail.com>
2015-01-14 00:54:39 +0000

    update to include st definition

--- colors/erosion.h
+++ colors/erosion.h
@@ -6,3 +6,34 @@
 
 #define base2  "#181512"  //selbgcolor
 #define base3  "#9a875f"  //selfgcolor
+
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+};
+
+static unsigned int defaultfg = 7;
+static unsigned int defaultbg = 0;
+static unsigned int defaultcs = 256;
